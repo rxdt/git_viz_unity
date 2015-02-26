ad80b85ebf15bec705d6f3dd730fc208dc864c4c
Roxana del Toro <rxdeltoro@gmail.com>
2015-02-25 18:54:57 -0800

    Added GameManager object and script to create random spheres

---
+++ Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	public GameObject sphere;
+
+	void Start(){
+		for (int i = 0; i < 100; i++) {
+			Instantiate(sphere);
+		}
+	}
+}
---
+++ Assets/RandomSphere.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSphere : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		transform.position = Random.insideUnitSphere * 100;
+	}
+}
