bcb52fca22e0f9993b3499beb6bdbca0226e0a64
Roxana del Toro <rxdeltoro@gmail.com>
2015-03-03 16:45:47 -0800

    WIP

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,13 +7,35 @@ public class GameManager : MonoBehaviour {
 	public GameObject sphere;
 	public GameObject Tree;
 	public Node root;
-
 	private static Vector3 ROOTLOCATION;
+	private static Dictionary<int, Dictionary<string, string>> children;
+
+	public int nodeCount = 10;
 
 	void Start(){
-		root = parseJSON (); // returns refernece to 'root' node of repo
-		renderTree ();
+		// the start of tree trunk and center of our environment - child of terrain and GameManager
 		ROOTLOCATION = new Vector3(0, 0, 0);
+
+		root = parseJSON (); // returns reference to 'root' node of repo
+		createNode(root);
+
+		while()
+//		renderTree ();
+		createToyTree();
+
+
+	}
+
+	/**
+	 * This creates a new node x,y,z distance from its parent.
+	 **/
+	public void createNode(Node parent){
+		Node node = new Node(null, root, "filename whatever");
+		Vector3 newNodeVector = root;
+	}
+
+	public void createToyTree(){
+
 	}
 
 	// returns root node of tree
