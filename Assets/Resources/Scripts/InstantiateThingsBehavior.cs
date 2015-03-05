8151e537901eb7e1f5fd94eed116f86aea9d623a
Roxana del Toro <rxdeltoro@gmail.com>
2015-03-05 12:16:15 -0800

    Reimplemented GameManager -> renamed to InitializeThingsBehavior and Node -> NodeBehavior. Correctly setup up relationships with NodePrefab

---
+++ Assets/Resources/Scripts/InstantiateThingsBehavior.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InstantiateThingsBehavior : MonoBehaviour {
+	
+	private static Vector3 ROOTLOCATION;
+	List<Dictionary<char, List<string>>> commits;
+
+	public GameObject NodePrefab; 
+	public List<GameObject> MyKids;
+	
+	void Start () {
+		//Instantiate a Node from NodePrefab var; 
+		GameObject jimmy = (GameObject)Instantiate(NodePrefab, new Vector3(-1.0f, 0, 0), Quaternion.identity);
+		//Keeping the gameobject reference from Jimmy for later use
+		MyKids.Add (jimmy);
+		//Instantiate a Bananas from SquarePrefab var;	
+		GameObject timmy = (GameObject)Instantiate(NodePrefab, new Vector3(0, 1.0f, 0), Quaternion.identity);
+		//Keeping the gameobject reference from Timmy for later use
+		MyKids.Add (timmy);
+		
+		//Accessing Jimmy NodeBehavior component because that's where his children list will be.
+		NodeBehavior j_behavior = jimmy.GetComponent<NodeBehavior> ();
+
+		//Adding timmy as child to jimmy's kids (var my_kids) (Note that Im just keeping their references, not actually setting them as kids)
+		j_behavior.my_kids.Add (timmy.transform);
+
+		//Actually setting jimmy's transform as a parent for timmy's transform otherwise they wont move. (Now Timmy is actually Jimmy's kid)
+		timmy.transform.SetParent (jimmy.transform);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+}
+
+
+
+
---
+++ Assets/Resources/Scripts/NodeBehavior.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NodeBehavior : MonoBehaviour {
+	public string my_path;
+	
+	public List<Transform> my_kids;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//A calls OnDestroy()
+	//A has B and C as kids
+	//B calls OnDestroy()
+	//D calls OnDestroy()
+	//D has no kids
+	//D suicides
+	//B continues the foreach
+	//B sees no kids
+	//B suicides
+	//C calls OnDestroy()
+	//C has no kids
+	//C suidides
+	//A no longer has kids
+	//A suicides
+	
+	//this will always be called whenever we call a Destroy() on a gameobject, before it is actually destroyed.
+	public void OnDestroy()
+	{
+		foreach (Transform kid_t in my_kids) {
+			//By destroying the object by calling the Destroy method below, the kid's OnDestroy() method will be called first.
+			//That's why we don't need to call OnDestroy() directly for the kid.
+			//NodeBehavior kid_b = kid_t.GetComponent<NodeBehavior>();
+			Destroy(kid_t.gameObject);
+			
+		}
+		//I dont actually need to call Destroy() on myself, because someone already, hence triggered OnDestroy()
+		//Destroy(this.gameObject);
+	}
+
+	
+//	public void createDummyCommitsList(){
+//		commit = new Dictionary<char, List<string>>();
+//		List<string> list = new List<string>();
+//		list.Add("file_one");
+//		list.Add("file_two");
+//		list.Add("file_three");
+//		commit['A'] = list;
+//	}
+}
